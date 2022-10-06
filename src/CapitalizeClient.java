bad42f92e3f9f20be59368267c810e389483205b
kuokyn <viktoria2nikiforova@gmail.com>
2022-10-06 22:18:44 +0300

    troubles with javac

--- src/CapitalizeClient.java
+++ src/CapitalizeClient.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class CapitalizeClient {
     public static void main(String[] args) throws Exception {
-        /*if (args.length != 1) {
-            System.err.println("Передайте IP-адрес сервера в качестве единственного аргумента командной строки");
-            return;
-        }*/
         try (var socket = new Socket("localhost", 59898)) {
             System.out.println("Введите строки текста, затем Ctrl + D или Ctrl + C, чтобы выйти");
             var scanner = new Scanner(System.in);
